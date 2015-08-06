155e2612a66fcc1c83656e00af4ee6d115a6f536
Slava <fender707@rambler.ru>
2015-08-06 16:37:58 +0300

    Added tests for sign up

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 feature 'Visitor signs up' do
   scenario 'with valid data' do
-    sign_in_with 'slava','fender707@rambler.ru', 'password', 'password'
+    sign_up_with 'slava','fender707@rambler.ru', 'password', 'password'
 
     expect(page).to have_content('Logout')
   end
 
   scenario 'with invalid email' do
-    sign_in_with 'slava','invalid_email', 'password', 'password'
+    sign_up_with 'slava','invalid_email', 'password', 'password'
 
     expect(page).to have_content('Sign up')
   end
 
   scenario 'with blank password' do
-    sign_in_with 'slava','invalid_email', '', ''
+    sign_up_with 'slava','invalid_email', '', ''
 
     expect(page).to have_content('Sign up')
   end
 
-  def sign_in_with( username, email, password, password_confirmation)
+  def sign_up_with( username, email, password, password_confirmation)
     visit new_user_registration_path
     fill_in 'Username', with: username
     fill_in 'Email', with: email
